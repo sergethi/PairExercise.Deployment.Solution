d8d2982160f83da0a5bc3dd2c2dda80b937a47af
SERGE <nsergearistide@gmail.com>
2020-06-10 23:17:39 -0400

    my first commit

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 const pkg = require('../../package.json')
 const dbName = process.env.NODE_ENV === 'test' ? `${pkg.name}-test` : pkg.name
-const dbUrl = process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`
+const dbUrl = process.env.DATABASE_URL || `postgres://serge:1234@localhost:5432/${dbName}`
 const client = new Sequelize(dbUrl, { logging: false, operatorsAliases: false })
 
 module.exports = client
